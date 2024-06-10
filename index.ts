c8fd9cfb076ab095f05026b3f826fa08688dc4d9
Matteo Bianchi <37507190+mbianchidev@users.noreply.github.com>
2024-06-10 19:28:27 +0200

    Update index.ts
    Changed CSS class for the list (to fix the app)

--- index.ts
+++ index.ts
@@ -21,7 +21,7 @@ const cache = new LRU({
 
 async function ghPinnedRepos(username: string) {
   const $ = await aimer(`https://github.com/${username}`);
-  const pinned = $(".pinned-item-list-item.public").toArray();
+  const pinned = $(".js-pinned-items-reorder-list").toArray();
 
   // if empty
   if (!pinned || pinned.length === 0) return [];
